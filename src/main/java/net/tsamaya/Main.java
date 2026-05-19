fd1b604237449bf03f5cd753232a93f786e88bbc
Arnaud Ferrand <arferrand@gmail.com>
2026-05-19 10:57:51 +0100

    wraps request with try..catch

--- src/main/java/net/tsamaya/Main.java
+++ src/main/java/net/tsamaya/Main.java
@@ -1,10 +1,12 @@
 package net.tsamaya;
 
+import com.opencagedata.jopencage.JOpenCageException;
 import com.opencagedata.jopencage.JOpenCageGeocoder;
 import com.opencagedata.jopencage.model.JOpenCageForwardRequest;
 import com.opencagedata.jopencage.model.JOpenCageResponse;
 import com.opencagedata.jopencage.model.JOpenCageResult;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.List;
 
 public class Main {
@@ -12,29 +14,33 @@ public class Main {
         System.out.println("Hello world!");
 
         String apiKey = System.getenv("OPENCAGE_API_KEY");
-        if(apiKey == null ){
+        if (apiKey == null) {
             System.out.println("Fail to read env var OPENCAGE_API_KEY");
             System.exit(1);
         }
         JOpenCageGeocoder jOpenCageGeocoder = new JOpenCageGeocoder(apiKey);
 
         Package p = jOpenCageGeocoder.getClass().getPackage();
-        System.out.println("Title: "+p.getImplementationTitle());
-        System.out.println("Version (imp): "+p.getImplementationVersion());
-        System.out.println("Version (spec): "+p.getSpecificationVersion());
-        System.out.println("Vendor: "+p.getImplementationVendor());
+        System.out.println("Title: " + p.getImplementationTitle());
+        System.out.println("Version (imp): " + p.getImplementationVersion());
+        System.out.println("Version (spec): " + p.getSpecificationVersion());
+        System.out.println("Vendor: " + p.getImplementationVendor());
 
         jOpenCageGeocoder.setHost("localhost");
         jOpenCageGeocoder.setHttpsEnabled(false);
 
-
         JOpenCageForwardRequest request = new JOpenCageForwardRequest("Lyon");
-        JOpenCageResponse response = jOpenCageGeocoder.forward(request);
-        List<JOpenCageResult> results = response.getResults();
-        if (results != null) {
-            results.forEach(result -> {
-                System.out.println((result.getFormatted()));
-            });
+        try {
+            JOpenCageResponse response = jOpenCageGeocoder.forward(request);
+            List<JOpenCageResult> results = response.getResults();
+            if (results != null) {
+                results.forEach(result -> {
+                    System.out.println((result.getFormatted()));
+                });
+            }
+        } catch (JOpenCageException e) {
+            System.out.println("Error during forward method");
+            e.printStackTrace();
         }
 
     }
